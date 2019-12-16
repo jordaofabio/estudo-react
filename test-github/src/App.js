c7d1ab02fd81ea29065b113fedee574da210d7f3
Fabio <fabiosjordao@gmail.com>
2019-12-16 00:05:01 -0300

    Separando os componentes

--- test-github/src/App.js
+++ test-github/src/App.js
@@ -1,39 +1,37 @@
 import React from 'react';
+import Search from './components/search';
+import UserInfo from './components/user-info';
+import Repos from './components/repos';
+import Actions from './components/actions';
 
 function App() {
   return (
     <div className="App">
-      <div className="search">
-        <input
-          type="search"
-          placeholder="Digite o nome do usuário no Github"
-        />
-      </div>
-      <div className="user-info">
-        <img src='https://avatars1.githubusercontent.com/u/24848934?v=4' />
-        <h1><a href="https://www.github.com/jordaofabio" target="_blank">Fabio Jordão</a></h1>
-      <ul className="repos-info">
-        <li>Repositórios: 50</li>
-        <li>Seguidores: 50</li>
-        <li>Seguindo: 50</li>
-      </ul>
+      <Search />
+      
+      <UserInfo />
+        
+      <Actions />
 
-      <div className="actions">
-        <button>Ver Repositórios</button>
-        <button>Ver Favoritos</button>
-      </div>
+      <Repos 
+        className="repos-container"
+        title="Repositórios"
+        repos={[{
+          name: 'Teste',
+          link: 'teste.html'
+        }]}
+      />
 
-      <ul className="repos">
-        <h2>Repositíos</h2>
-        <li><a href="#">Nome do repositório</a></li>
-      </ul>
+      <Repos 
+        className="starred"
+        title="Favoritos"
+        repos={[{
+          name: 'Teste',
+          link: 'teste.html'
+        }]}
+      />
 
-      <ul className="starred">
-        <h2>Favoritos</h2>
-        <li><a href="#">Nome do repositório</a></li>
-      </ul>
-      </div>
-    </div>
+  </div>
   );
 }
 
---
+++ test-github/src/components/user-info.js
@@ -0,0 +1,17 @@
+'use strict'
+
+import React from 'react'
+
+const UserInfo = () => (
+    <div className="user-info">
+        <img src='https://avatars1.githubusercontent.com/u/24848934?v=4' />
+        <h1><a href="https://www.github.com/jordaofabio" target="_blank">Fabio Jordão</a></h1>
+          <ul className="repos-info">
+            <li>Repositórios: 50</li>
+            <li>Seguidores: 50</li>
+            <li>Seguindo: 50</li>
+          </ul>
+          </div>
+);
+
+export default UserInfo;
